b57ae400686a49da0c033c397b92fdb3952d62e2
Rangga Bagas Santoso <rangga.bagas0@gmail.com>
2018-05-15 17:46:05 +0700

    add column SITE_ID_CUSTOMER in patch v9

---
+++ app/src/main/java/com/sap/inspection/model/migrate/GeneralPatch9.java
@@ -0,0 +1,32 @@
+package com.sap.inspection.model.migrate;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.sap.inspection.model.DbManager;
+
+public class GeneralPatch9 extends DBPatch {
+
+    /**
+     * 8 May 2018
+     * add column site_id_customer to Site table
+     * */
+    @Override
+    public void apply(SQLiteDatabase db) {
+        Log.d(getClass().getName(), "general patch 9");
+        db.execSQL("ALTER TABLE "+ DbManager.mSite+" ADD COLUMN "+DbManager.colSiteIdCustomer+" VARCHAR");
+    }
+
+    @Override
+    public void revert(SQLiteDatabase db) {
+        Log.d(getClass().getName(), "revert patch to 8");
+        db.execSQL("ALTER TABLE " + DbManager.mSite + " RENAME TO " + DbManager.mSite +"temp");
+        DbManager.createDB(db);
+        db.execSQL("INSERT INTO " + DbManager.mSite + " SELECT "
+                + DbManager.colID
+                + DbManager.colName
+                + DbManager.colSiteLocation
+                + " FROM " + DbManager.mSite +"temp");
+        db.execSQL("DROP TABLE " + DbManager.mSite+"temp");
+    }
+}
