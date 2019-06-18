5b325dc10208ca79f54798272893b59a977dff97
Rangga <rangga.bagas0@gmail.com>
2019-06-18 17:26:24 +0700

    add shouldRelogin flag to do relogin after deleting data

--- app/src/main/java/com/sap/inspection/event/DeleteAllProgressEvent.java
+++ app/src/main/java/com/sap/inspection/event/DeleteAllProgressEvent.java
@@ -2,8 +2,11 @@ package com.sap.inspection.event;
 
 public class DeleteAllProgressEvent extends ProgressEvent{
 
-	public DeleteAllProgressEvent(String progress, boolean done) {
+	public boolean shouldRelogin;
+
+	public DeleteAllProgressEvent(String progress, boolean done, boolean shouldRelogin) {
 		super(progress, done);
+		this.shouldRelogin = shouldRelogin;
 	}
 
 	public DeleteAllProgressEvent(String progress) {
