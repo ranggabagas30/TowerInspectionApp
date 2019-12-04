6405ddad4fdbc9479ba3fdd1c516172a7325416d
Rangga <rangga.bagas@domikado.com>
2019-12-04 10:23:06 +0700

    using telephony IMEI, Secure.AndroidID, Wifi Mac Address, or FirebaseInstanceID as android id associated to device

--- app/src/main/java/com/sap/inspection/tools/AndroidUID.java
+++ app/src/main/java/com/sap/inspection/tools/AndroidUID.java
@@ -3,30 +3,34 @@ package com.sap.inspection.tools;
  * Created by Rindang Septyan
  */
 
-import android.app.Activity;
+import android.Manifest;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.preference.PreferenceManager;
+import android.provider.Settings;
+import android.support.annotation.RequiresPermission;
 import android.telephony.TelephonyManager;
 
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.pixplicity.easyprefs.library.Prefs;
+import com.sap.inspection.BuildConfig;
 import com.sap.inspection.R;
 
 import java.util.Calendar;
 import java.util.Random;
 
 public class AndroidUID {
-	Activity activity;
-	public AndroidUID(Activity activity) {
-		this.activity = activity;
-	}
-	public static String UID;
+	private static String UID;
 
-	public static  String getUID(Context context){
+	/**
+	 * @deprecated
+	 * */
+	public static String getUID(Context context){
 		if (UID != null)
 			return UID;
-
 		TelephonyManager telephonyManager = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
 		UID = telephonyManager.getDeviceId();
 		if (UID != null)
@@ -49,4 +53,32 @@ public class AndroidUID {
 		return UID;
 	}
 
+	@RequiresPermission(Manifest.permission.READ_PHONE_STATE)
+	public static String getDeviceID(Context context) {
+
+		// UID has been assigned, return it
+		if (UID != null) return UID;
+
+		TelephonyManager telephony = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+		if (telephony.getDeviceId() != null){ // use IMEI
+			UID = telephony.getDeviceId();
+		}else{
+			// else use Secure.AndroidID
+			UID = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+		}
+
+		if (UID == null) {
+			WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+			if (wifiManager != null) {
+				WifiInfo wInfo = wifiManager.getConnectionInfo();
+				if (wInfo != null) // use mac address
+					UID = wInfo.getMacAddress();
+			}
+		}
+
+		if (UID == null) { // use Firebase Instance ID
+			UID = Prefs.getString(context.getString(R.string.app_fcm_reg_id), null);
+		}
+		return UID;
+	}
 }
